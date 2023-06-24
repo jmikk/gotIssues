a337314edfccdd80949dd875c88d2174a6f98e05
jmikk <buldog.buldog26@gmail.com>
2023-06-24 12:50:39 -0500

    Update issues.py

--- issues.py
+++ issues.py
@@ -6,15 +6,23 @@ import os
 import sys
 import re
 
-Version = "6.0"
+Version = "5.6"
 print("GotIssues version "+str(Version))
 
 UserAgent=input("Please enter your main nation's name: ")
 filename="puppet.csv"
+Pulleventmode="no"
+Pulleventmode=Pulleventmode.lower().replace(" ","_")
+
+
+if(Pulleventmode=='y'):
+	Pulleventmode='yes';
+
 
 names=[]
 password=[]
 NewListOfIssues = 'link_list.txt'
+NewListOfPacks = 'pack_list.txt'
 with open("puppet.csv") as csv_file:
 	csv_reader = csv.reader(csv_file)
 	for row in csv_reader:
@@ -24,19 +32,46 @@ index=0
 if os.path.exists(NewListOfIssues):
   os.remove(NewListOfIssues)
 
+if os.path.exists(NewListOfPacks):
+  os.remove(NewListOfPacks)
+
 for every in names:
-	every=every.replace(" ", "_").lower()
+	every=every.replace(" ", "_")
 #	print(every)
 #	print(password[index])
 	
-	r = requests.get('https://www.nationstates.net/cgi-bin/api.cgi/', headers={'User-Agent': UserAgent, 'X-Password': password[index].replace(" ","_")}, params={'nation':every, 'q':'issuesummary'})
+	r = requests.get('https://www.nationstates.net/cgi-bin/api.cgi/', headers={'User-Agent': UserAgent, 'X-Password': password[index].replace(" ","_")}, params={'nation':every, 'q':'issues+packs'})
 	sleep(.7)
 	print("Grabbing " + every + "; this can take a while for the server hamsters to give it to the API Gnomes.")
 	soup = BeautifulSoup(r.content, "xml")
-	for ISSUEid in soup.find_all('ISSUE'):
-		print(every)
-		with open(NewListOfIssues, 'a+') as f:
-			f.write("https://www.nationstates.net/container={}/nation={}/page=show_dilemma/dilemma={}/template-overall=none/x-enabled-by=gotIssues\n".format(every, every, ISSUEid.get('id')))
-	index=index+1
+	with open(NewListOfPacks, "a+") as h:
+		count=0
+		for toOpenPACKS in soup.find_all('PACKS'):
+			#input(toOpenPACKS.text)
+			while(count < int(toOpenPACKS.text)):
+				count= count+1
+				h.writelines(f"https://www.nationstates.net/page=deck/nation={every}/?open_loot_box=1/autoclose=1\n")
+		for ISSUEid in soup.find_all('ISSUE'):
+			print(every)
+			with open(NewListOfIssues, 'a+') as f:
+				if(ISSUEid.get('id')=='407'):
+					if(Pulleventmode != "yes"):
+						f.writelines('https://www.nationstates.net/page=show_dilemma/dilemma=407/template-overall=none'+"/nation="+every+"/container="+every+"/template-overall=none/pulleventmode=true\n")
+						print("issue 407")
+					else:
+						print("issue 407")
+						f.writelines('https://www.nationstates.net/page=show_dilemma/dilemma=407/template-overall=none'+"/nation="+every+"/container="+every+"/template-overall=none\n")
+				else:
+					if(Pulleventmode != "yes"):
+						print(ISSUEid.get('id'))
+						print(ISSUEid.OPTION.get('id'))
+						f.writelines('https://www.nationstates.net/page=enact_dilemma/choice-'+ISSUEid.OPTION.get('id')+'=1/dilemma='+ISSUEid.get('id')+"/nation="+every+"/container="+every+"/template-overall=none\n")
+					else:
+						print(ISSUEid.get('id'))
+						print(ISSUEid.OPTION.get('id'))
+						f.writelines('https://www.nationstates.net/page=enact_dilemma/choice-'+ISSUEid.OPTION.get('id')+'=1/dilemma='+ISSUEid.get('id')+"/nation="+every+"/container="+every+"/template-overall=none/pulleventmode=true\n")
+			#print('{}'.format(options.get('id')))
+			#print('{}'.format(ISSUEid.get('id')))           
+		index=index+1
 	
 print("Done, thanks for using GotIssues")
