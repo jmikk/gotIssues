d3f76b1825bde1837484ab7e55b17cb622af37c1
Kractero <115837402+Kractero@users.noreply.github.com>
2024-02-12 17:09:53 -0700

    Close the enacted issue page

--- autoclose=1.user.js
+++ autoclose=1.user.js
@@ -2,16 +2,18 @@
 // @name         autoclose=1
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
-// @author       9003
-// @match         *://*/*autoclose=1
+// @match        *://*/*autoclose=1
+// @match        https://www.nationstates.net/*page=enact_dilemma*
 // @exclude      https://www.nationstates.net/*page=show_dilemma*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=nationstates.net// 
-// @grant        none
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=nationstates.net//
+// @grant        window.close
 // ==/UserScript==
 
-(function() {
+// @match on autoclose=1 not necessary for gotissues but is for junkdajunk and others
+// @match on enact_dilemma as autoclose does not carry over to the issue answered screen
+// @exclude on show_dilemma since autoclose should not close the new intermediary screen
+
+(function () {
     'use strict';
     window.close();
-    // Your code here...
 })();
